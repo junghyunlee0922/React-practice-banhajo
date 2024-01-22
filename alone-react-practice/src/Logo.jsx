38f94507dc271d06cc08b8ad08f6779802846ff5
junghyunlee0922 <self21c123@naver.com>
2024-01-23 03:50:53 +0900

    데이터 바인딩

--- alone-react-practice/src/Logo.jsx
+++ alone-react-practice/src/Logo.jsx
@@ -1,6 +1,23 @@
+const logoImage = {
+  src: "/react.svg",
+  alt: "리액트",
+};
+
+const figureStyle = {marginBlock: 0, display: "flex", alignItems: "center", gap: 12}
+const figcaptionStyle = { whiteSpace: "nowrap" }
+
 // Logo함수 component
 function Logo() {
-  return <img src="/react.svg" alt="리액트" />;
+  return (
+    <figure style={figureStyle}>
+      <img src={logoImage.src} alt="" />
+      <figcaption style={figcaptionStyle}>{logoImage.alt}</figcaption>
+    </figure>
+  );
 }
 
 export default Logo;
+
+// 인라인 style을 설정하는 style prop의 경우
+// 바깥의 중괄호는 데이터를 연결시켜주기위한 JSX의 중괄호 구문이고
+// 안쪽은 중괄호는 자바스크립트의 style객체이다.
