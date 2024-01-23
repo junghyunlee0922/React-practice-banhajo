0f18187a4779d18d847cd1db8b93dab2a7580da3
junghyunlee0922 <self21c123@naver.com>
2024-01-23 17:48:22 +0900

    조건부 렌더링

--- alone-react-practice/src/Logo.jsx
+++ alone-react-practice/src/Logo.jsx
@@ -8,16 +8,30 @@ const figcaptionStyle = { whiteSpace: "nowrap" }
 
 // Logo함수 component
 function Logo() {
+  const onlyRenderImage = true;
+  // const onlyRenderImage = false; // 거짓일때 조건문 작동 안함.
+
+  // if (onlyRenderImage) {
+  //   return <img src={logoImage.src} alt="" />;
+  // }
+
+  // 삼항식으로 대신
+  // return onlyRenderImage ? <img src={logoImage.src} alt="" /> : (
+  //   <figure style={figureStyle}>
+  //     <img src={logoImage.src} alt="" />
+  //     <figcaption style={figcaptionStyle}>{logoImage.alt}</figcaption>
+  //   </figure>
+  // );
+
+  // 논리 연산자
   return (
     <figure style={figureStyle}>
       <img src={logoImage.src} alt="" />
-      <figcaption style={figcaptionStyle}>{logoImage.alt}</figcaption>
+      {onlyRenderImage || <figcaption style={figcaptionStyle}>{logoImage.alt}</figcaption>}
     </figure>
   );
+
+  
 }
 
 export default Logo;
-
-// 인라인 style을 설정하는 style prop의 경우
-// 바깥의 중괄호는 데이터를 연결시켜주기위한 JSX의 중괄호 구문이고
-// 안쪽은 중괄호는 자바스크립트의 style객체이다.
